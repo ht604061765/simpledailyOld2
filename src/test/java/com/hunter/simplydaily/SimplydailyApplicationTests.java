99ddc1c95bc31d6fd60a11f83706a23327daa138
hantao <15943286270@163.com>
2020-08-12 08:59:22 +0800

    初始提交

---
+++ src/test/java/com/hunter/simplydaily/SimplydailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunter.simplydaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplydailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
